2c6a2e499c33661130fb7f4264219a6c9fbbb111
Pygee <toni.pylvalainen@vr.fi>
2020-07-07 10:04:29 +0300

    push

---
+++ client/build/precache-manifest.013c4e3b09cd2f35139a0869172dcb8c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9284a857f0e8074bfe0eda1e06b16de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "117df500e081d49e49d6",
+    "url": "/static/css/main.aac8a10f.chunk.css"
+  },
+  {
+    "revision": "4faf9becf6f00029f035",
+    "url": "/static/js/2.b2bb3b02.chunk.js"
+  },
+  {
+    "revision": "0f59aded427042d87c15d0910a90fb3b",
+    "url": "/static/js/2.b2bb3b02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "117df500e081d49e49d6",
+    "url": "/static/js/main.97a44129.chunk.js"
+  },
+  {
+    "revision": "9b08c34181f0e6d88ba1",
+    "url": "/static/js/runtime-main.a5812c7e.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/cart.30f07071.svg"
+  },
+  {
+    "revision": "df704cf504395e9ead0ae1921b1fe490",
+    "url": "/static/media/crown.df704cf5.svg"
+  }
+]);
